8b78de6ed7eea94bbdeeaff692b978e03e41c90e
KanaiGooding <kgooding@hawaii.edu>
2021-02-16 23:07:46 -1000

    Inital

---
+++ src/checkers/board/Piece.java
@@ -0,0 +1,11 @@
+package checkers.board;
+
+import java.util.ArrayList;
+
+public class Piece {
+  
+  boolean isWhite;
+  String color = "";
+  ArrayList<Move> moves = new ArrayList<Move>();
+
+}
---
+++ src/checkers/board/Player.java
@@ -0,0 +1,5 @@
+package checkers.board;
+
+public class Player {
+
+}
