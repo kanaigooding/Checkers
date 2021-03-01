16e94c836d64b4d9188f0fb323aac884d674b73b
KanaiGooding <kgooding@hawaii.edu>
2021-02-28 18:38:44 -1000

    Update Board.java
    Checkers

--- src/checkers/board/Board.java
+++ src/checkers/board/Board.java
@@ -1,44 +1,48 @@
 package checkers.board;
 
 public class Board {
-  
+
   public String boardFEN = "";
   public Piece boardArr[][];
-  
+
   public Board() {
     this.boardFEN = "pppppppp/pppppppp/8/8/8/8/PPPPPPPP/PPPPPPPP";
-    this.boardArr= new Piece[8][8];
+    this.boardArr = new Piece[8][8];
   }
-  
+
+
   public String arrayToFen(Piece[][] arr) {
-    
+
     return "";
   }
-  
+
+
   public Piece[][] fenToArray() {
-    
+
     return new Piece[8][8];
   }
-  
+
+
   public void checkWinner() {
-    if(!arrayToFen(boardArr).contains("p")) {
+    if (!arrayToFen(boardArr).contains("p")) {
       blackWins();
-     }
-    
-    if(!arrayToFen(boardArr).contains("P")) {
+    }
+
+    if (!arrayToFen(boardArr).contains("P")) {
       whiteWins();
     }
-    
-   
+
   }
-  
+
+
   private void whiteWins() {
-    
+
   }
-  
+
+
   private void blackWins() {
-    
+
   }
-  
+
 
 }
